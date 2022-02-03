35b598770c32adbdfb45a5bd6a52fe99ae26748e
Arpit-Barjati <arpitbarjati90@gmail.com>
2022-02-03 21:22:28 +0530

    initial

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <h6 className="text-center my-4">&copy; NewsOutlet - Arpit Barjati</h6>
+        </div>
+    );
+}
+
+export default Footer;
